d70fd45d17fe05a56cf2cf1d6f18fba8e7115c14
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-21 17:53:07 -0600

    determine if person enters

--- text_game/intro.py
+++ text_game/intro.py
@@ -1,5 +1,15 @@
 def welcome_message():
     print("DO NOT ENTER")
 
+def person_enters():
+    person_enter = input("Are you up for a challenge? (y/n):")
+    if person_enter == "y":
+        return True
+    else:
+        return False
 
 welcome_message()
+if person_enters():
+   print("Welcome!")
+else:
+   print("come back soon!")
