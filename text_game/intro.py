ee12d4a3f57c89c3813c5ad5f3762fb13cfae770
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-21 18:14:56 -0600

    show activities

--- text_game/intro.py
+++ text_game/intro.py
@@ -8,8 +8,15 @@ def person_enters():
     else:
         return False
 
+def show_activities():
+    print("-----------------------")
+    print("What you wanna do?")
+    print("A. Swim with Sharks")
+    print("B. Dance with Wolves")
+    print("C. Sleep with Fish")
+
 welcome_message()
 if person_enters():
-   print("Welcome!")
+    show_activities()
 else:
    print("come back soon!")
