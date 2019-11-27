3611f2e83d82cccfac1199480be5b507ec7b2f4d
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-27 17:26:24 -0600

    add examples to sum, min, and max

---
+++ built_in/enumerators.py
@@ -0,0 +1,14 @@
+nums = [10, 20, 55, 100]
+
+largest = max(nums)
+smallest = min(nums)
+print(largest)
+print(smallest)
+
+# total = 0
+# for num in nums:
+#     total += num
+
+# total = sum(nums)
+    
+# print(total)
