8234e7473228971f08e137aacc5b48a9b94421b0
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-28 00:25:08 -0600

    add examples of all built in function

--- built_in/enumerators.py
+++ built_in/enumerators.py
@@ -1,14 +1,35 @@
-nums = [10, 20, 55, 100]
+# nums = [10, 20, 55, 100]
 
-largest = max(nums)
-smallest = min(nums)
-print(largest)
-print(smallest)
+# largest = max(nums)
+# smallest = min(nums)
+# # print(largest)
+# # print(smallest)
 
-# total = 0
-# for num in nums:
-#     total += num
+# # total = 0
+# # for num in nums:
+# #     total += num
 
-# total = sum(nums)
+# # total = sum(nums)
     
-# print(total)
+# # print(total)
+
+# nums = [10, 20, 55, 100]
+
+# def is_even(num):
+#     return num % 2 == 0
+
+# are_all_even = all(is_even(num) for num in nums)
+# print(are_all_even)
+
+
+
+
+
+# name = "ALEX"
+# all_upper = all(letter.isupper() for letter in name)
+# print(all_upper)
+
+passwords = ["pwd1234", "ADMIN", "password", "12345", "!!!!!?", "$$$$$$$$$$"]
+
+safe_passwords = sum(password.isupper() for password in passwords)
+print(safe_passwords)
